f89599c41b2385b0eb1fe3dc6ed75b08121c1d15
Carlos Toledo <carlombo@hotmail.com>
2024-06-26 23:03:52 -0400

    [ADD] Se crean funciones crear_artista, crea_grupo, relacion_artista_grupo, agregar_album

--- main/services.py
+++ main/services.py
@@ -1,14 +1,21 @@
-def crear_artista():
-    pass
+from main.models import *
 
-def crear_grupo():
-    pass
+def crear_artista(nombre, apellido, cantante, instrumento):
+    artista = Artista(nombre=nombre, apellido=apellido, cantante=cantante, instrumento=instrumento)
+    artista.save()
+    return artista
 
-def relacion_artista_grupo():
-    pass
+def crear_grupo(nombre, fecha_creacion):
+    grupo = Grupo(nombre=nombre, fecha_creacion=fecha_creacion)
+    grupo.save()
 
-def agregar_album():
-    pass
+def relacion_artista_grupo(artista, grupo, fecha_ingreso, creacion_registro, agregado_por):
+    relacion = ArtistaGrupo(artista=artista, grupo=grupo, fecha_ingreso=fecha_ingreso, creacion_registro=creacion_registro, agregado_por=agregado_por)
+    relacion.save()
+
+def agregar_album(titulo, year, grupo_id):
+    album = Album(titulo=titulo, year=year, grupo_id=grupo_id)
+    album.save()
 
 def obtiene_artista():
     pass
@@ -24,3 +31,10 @@ def artista_participa_albumes():
 
 def grupo_albumes():
     pass
+
+
+'''
+TIP: En las funciones en que creamos un objeto, sería una buena práctica para
+este ejemplo, retornar el elemento creado, con el fin de obtener los índices para
+crear los otros registros.
+'''
