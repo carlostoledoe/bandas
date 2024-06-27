203298016aa8a9ab04524f931f7e82d83891f040
Carlos Toledo <carlombo@hotmail.com>
2024-06-26 22:05:50 -0400

    [ADD] Se crean modelos Artista, Grupo, Album y ArtistaGrupo

--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Artista(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    cantante = models.BooleanField(default=False)
+    instrumento = models.CharField(max_length=50)
+
+class Grupo(models.Model):
+    nombre = models.CharField(max_length=50)
+    fecha_creacion = models.DateField()
+    artista = models.ManyToManyField(Artista, related_name='grupos', through='ArtistaGrupo')
+
+class Album(models.Model):
+    titulo = models.CharField(max_length=50)
+    year = models.IntegerField()
+    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
+
+class ArtistaGrupo(models.Model):
+    artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
+    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
+    fecha_ingreso = models.DateField()
+    creacion_registro = models.DateField()
+    agregado_por = models.CharField(max_length=50)
