d6f0d0e0aa52ca1c03dd1ae76403d4397b635ff4
Carlos Toledo <carlombo@hotmail.com>
2024-06-27 20:04:28 -0400

    [UPDATE] se actualizan funciones y modelo

--- main/models.py
+++ main/models.py
@@ -6,20 +6,28 @@ class Artista(models.Model):
     apellido = models.CharField(max_length=50)
     cantante = models.BooleanField(default=False)
     instrumento = models.CharField(max_length=50)
+    def __str__(self) -> str:
+        return f'{self.nombre} {self.apellido}'
 
 class Grupo(models.Model):
     nombre = models.CharField(max_length=50)
     fecha_creacion = models.DateField()
     artista = models.ManyToManyField(Artista, related_name='grupos', through='ArtistaGrupo')
+    def __str__(self) -> str:
+        return f'{self.nombre}'
 
 class Album(models.Model):
     titulo = models.CharField(max_length=50)
     year = models.IntegerField()
     grupo = models.ForeignKey(Grupo, related_name='albumes', on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return f'{self.titulo} {self.year} {self.grupo}'
 
 class ArtistaGrupo(models.Model):
     artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
+    # artista_id int
     grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
+    # grupo_id int
     fecha_ingreso = models.DateField()
     creacion_registro = models.DateField(auto_now_add=True)
     agregado_por = models.CharField(max_length=50)
--- main/services.py
+++ main/services.py
@@ -1,24 +1,27 @@
 from main.models import *
 
-def crear_artista(nombre, apellido, cantante, instrumento):
+def crear_artista(nombre:str, apellido:str, cantante:bool, instrumento:str):
     artista = Artista(nombre=nombre, apellido=apellido, cantante=cantante, instrumento=instrumento)
     artista.save()
     return artista
 
-def crear_grupo(nombre, fecha_creacion):
+def crear_grupo(nombre:str, fecha_creacion:str):
     grupo = Grupo(nombre=nombre, fecha_creacion=fecha_creacion)
     grupo.save()
+    return grupo
 
-def relacion_artista_grupo(artista, grupo, fecha_ingreso, creacion_registro, agregado_por):
-    relacion = ArtistaGrupo(artista=artista, grupo=grupo, fecha_ingreso=fecha_ingreso, creacion_registro=creacion_registro, agregado_por=agregado_por)
+def relacion_artista_grupo(artista_id:int, grupo_id:int, fecha_ingreso:str, agregado_por:str):
+    relacion = ArtistaGrupo(artista=artista_id, grupo=grupo_id, fecha_ingreso=fecha_ingreso, agregado_por=agregado_por)
     relacion.save()
 
-def agregar_album(titulo, year, grupo_id):
+def agregar_album(titulo:str, year:int, grupo_id:int):
     album = Album(titulo=titulo, year=year, grupo_id=grupo_id)
     album.save()
+    print(album)
 
-def obtiene_artista():
-    pass
+def obtiene_artistas(nombre:str):
+    artistas = Artista.objects.filter(nombre__contains=nombre) # Contiene
+    print(artistas)
 
 def obtiene_grupo():
     pass
